c7caccaab65317519139c319436f33e90bb3bc8e
Michael Dermksian <mdermksian@gmail.com>
2019-04-20 11:41:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#Spotify Tracker
+
+To run the MongoDB server locally, run command:
+```
+mongod --dbpath D:\data\db
+```
+
+To connect via the shell:
+```
+mongo
+```
+Although compass or robo are more useful...
+
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require( 'express' );
+const app = express();
+const PORT = 3000;
+
+app.use( ( request, response, next ) => {
+    console.log( request.headers );
+    next();
+} );
+  
+app.use( ( request, response, next ) => {
+    request.chance = Math.random();
+    next();
+} );
+
+app.get( '/', ( request, response ) => {
+    response.json({
+        chance: request.chance
+    });
+} )
+
+app.listen( PORT );
